72ab4b68de9991e6c748c6b508c0468ae8432c58
ErzhanSou <souverigh1@gmail.com>
2022-02-22 02:35:50 +0530

    made two bot section, personalPortal and ProcessContol

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,13 +2,15 @@ import React from 'react';
 import Education from "./Tasks/Location/Tools/Education";
 import Ecology from "./Tasks/Location/Tools/Ecology";
 import TasksMenu from "./Tasks/TasksMenu";
+import PersonalPortal from "./Tasks/Location/PersonalPortal/PersonalPortal";
+import ProcessControl from "./Tasks/Location/ProcessControl/ProcessControl";
 
 const Homepage = () => {
 
     /*Record components params to array of objects*/
     const tools = [
         {
-            action_link: '/Education',
+            action_link: '/',
             action_title: 'Обучение',
             action: <Education />
         },
@@ -18,7 +20,22 @@ const Homepage = () => {
             action: <Ecology />
         }
     ]
-
+    /*Record components params to array of object*/
+    const personalPortal = [
+        {
+            action_link: '/',
+            action_title: 'Персональный портал',
+            action: <PersonalPortal />
+        }
+    ]
+    /*Record components params to array of object*/
+    const processControl = [
+        {
+            action_link: '/',
+            action_title: 'Управление процессом',
+            action: <ProcessControl />
+        }
+    ]
 
 
     return (
@@ -27,6 +44,11 @@ const Homepage = () => {
                 <div className="main__top">
                     {/*transfer array "tools"*/}
                     <TasksMenu actions={tools} minW={'100%'} minH={"370px"} />
+                </div>
+                {/*transfer array "personalPortal and processControl"*/}
+                <div className="main__bot">
+                    <TasksMenu actions={personalPortal} minW={'808px'} minH={'323px'}/>
+                    <TasksMenu actions={processControl} minW={'358px'} minH={'323px'}/>
                 </div>
             </div>
         </div>
